3b56e50fc2992edeb2b00dab6fcfbcdc560c1852
Nathalye jessica <rafaelamamaeteama@gmail.com>
2024-09-11 13:17:09 +0000

    impar ou par

---
+++ par_impar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Digite um número:\n");
+    scanf("%d",&num);
+    if(num % 2 == 0){
+        printf("O número digitado é par:\n");
+    }
+    else{
+        printf("O número digitado é impar:\n");
+
+    }
+    return 0;
+
+}
